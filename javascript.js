d147bee6028541e883c3e422d760237761426145
GoodGuyFate <xgamerfatex@gmail.com>
2026-01-04 12:48:14 +0200

    Create the initial grid that is able to be resized

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
 const grid = document.querySelector(".grid-container")
-const box = document.createElement("div")
-box.classList.add('grid-item')
-grid.appendChild(box)
+
+
+const gridSize = 7
+
+for (j = 0; j < gridSize; j++) {
+
+    const row = document.createElement("div")
+    row.classList.add('row-container')
+    
+    for (let i = 0; i < gridSize; i++) {
+        const box = document.createElement("div")
+        box.classList.add('grid-item')
+
+        row.appendChild(box)
+    }
+    grid.appendChild(row)
+}
+
+
+
+
+
